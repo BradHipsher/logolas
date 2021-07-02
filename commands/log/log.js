b2c6dc1e903d3b0ccab822e06c320e80e9df1588
Brad Hipsher <bhipsher07@gmail.com>
2021-07-02 18:20:44 -0500

    trim log entries

--- commands/log/log.js
+++ commands/log/log.js
@@ -20,16 +20,16 @@ module.exports = {
 		var p9 = "";
 		var p10 = "";
 
-		if (args[2]) p1 = args[2];
-		if (args[3]) p2 = args[3];
-		if (args[4]) p3 = args[4];
-		if (args[5]) p4 = args[5];
-		if (args[6]) p5 = args[6];
-		if (args[7]) p6 = args[7];
-		if (args[8]) p7 = args[8];
-		if (args[9]) p8 = args[9];
-		if (args[10]) p9 = args[10];
-		if (args[11]) p10 = args[11];
+		if (args[2]) p1 = args[2].trim();
+		if (args[3]) p2 = args[3].trim();
+		if (args[4]) p3 = args[4].trim();
+		if (args[5]) p4 = args[5].trim();
+		if (args[6]) p5 = args[6].trim();
+		if (args[7]) p6 = args[7].trim();
+		if (args[8]) p7 = args[8].trim();
+		if (args[9]) p8 = args[9].trim();
+		if (args[10]) p9 = args[10].trim();
+		if (args[11]) p10 = args[11].trim();
 
 		try {
 			// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
