8fe277e4d84ab19671fdc2d5d6a3eca1f714d40e
Brad Hipsher <bhipsher07@gmail.com>
2021-07-02 00:13:46 -0500

    get-log functionality

--- commands/log/get-log.js
+++ commands/log/get-log.js
@@ -9,7 +9,20 @@ module.exports = {
 		// equivalent to: SELECT * FROM tags WHERE name = 'tagID' LIMIT 1;
 		const tag = await Tags.findOne({ where: { randid: tagID } });
 		if (tag) {
-			return message.channel.send("Game: " + tag.get('gameName'));
+			return message.channel.send(
+				"Game: " + tag.get('gameName') + "\n" + 
+				"Date: " + tag.get('date') + "\n" +
+				"Player1: " + tag.get('player1') + "\n" +
+				"Player2: " + tag.get('player2') + "\n" +
+				"Player3: " + tag.get('player3') + "\n" +
+				"Player4: " + tag.get('player4') + "\n" +
+				"Player5: " + tag.get('player5') + "\n" +
+				"Player6: " + tag.get('player6') + "\n" +
+				"Player7: " + tag.get('player7') + "\n" +
+				"Player8: " + tag.get('player8') + "\n" +
+				"Player9: " + tag.get('player9') + "\n" +
+				"Player10: " + tag.get('player10')
+				);
 		}
 		return message.reply(`Could not find tag: ${tagID}`);
 	},
