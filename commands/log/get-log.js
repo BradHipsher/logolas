fa901bcd4e08780918e01ad9ea660d9c7e582985
Brad Hipsher <bhipsher07@gmail.com>
2021-07-02 21:16:17 -0500

    get-log functionality

--- commands/log/get-log.js
+++ commands/log/get-log.js
@@ -11,19 +11,33 @@ module.exports = {
 		if (args[0] === "all") {
 
 			// jank implementation
-			const rowlist = await Tags.findAll({ attributes: ['rowid'] }).map(t => t.rowid);
-			const gamelist = await Tags.findAll({ attributes: ['gameName'] }).map(t => t.gameName);
-			const datelist = await Tags.findAll({ attributes: ['date'] }).map(t => t.date);
-			const player1list = await Tags.findAll({ attributes: ['player1'] }).map(t => t.player1);
-			const player2list = await Tags.findAll({ attributes: ['player2'] }).map(t => t.player2)
-			const player3list = await Tags.findAll({ attributes: ['player3'] }).map(t => t.player3);
-			const player4list = await Tags.findAll({ attributes: ['player4'] }).map(t => t.player4);
-			const player5list = await Tags.findAll({ attributes: ['player5'] }).map(t => t.player5);
-			const player6list = await Tags.findAll({ attributes: ['player6'] }).map(t => t.player6);
-			const player7list = await Tags.findAll({ attributes: ['player7'] }).map(t => t.player7);
-			const player8list = await Tags.findAll({ attributes: ['player8'] }).map(t => t.player8);
-			const player9list = await Tags.findAll({ attributes: ['player9'] }).map(t => t.player9);
-			const player10list = await Tags.findAll({ attributes: ['player10'] }).map(t => t.player10);
+			const rowlist = await Tags.findAll({ attributes: ['rowid'] });
+			const gamelist = await Tags.findAll({ attributes: ['gameName'] });
+			const datelist = await Tags.findAll({ attributes: ['date'] });
+			const player1list = await Tags.findAll({ attributes: ['player1'] });
+			const player2list = await Tags.findAll({ attributes: ['player2'] })
+			const player3list = await Tags.findAll({ attributes: ['player3'] });
+			const player4list = await Tags.findAll({ attributes: ['player4'] });
+			const player5list = await Tags.findAll({ attributes: ['player5'] });
+			const player6list = await Tags.findAll({ attributes: ['player6'] });
+			const player7list = await Tags.findAll({ attributes: ['player7'] });
+			const player8list = await Tags.findAll({ attributes: ['player8'] });
+			const player9list = await Tags.findAll({ attributes: ['player9'] });
+			const player10list = await Tags.findAll({ attributes: ['player10'] });
+
+			rowlist = rowlist.map(t => t.rowid);
+			gamelist = gamelist.map(t => t.gameName);
+			datelist = datelist.map(t => t.date);
+			player1list = player1list.map(t => t.player1);
+			player2list = player2list.map(t => t.player2)
+			player3list = player3list.map(t => t.player3);
+			player4list = player4list.map(t => t.player4);
+			player5list = player5list.map(t => t.player5);
+			player6list = player6list.map(t => t.player6);
+			player7list = player7list.map(t => t.player7);
+			player8list = player8list.map(t => t.player8);
+			player9list = player9list.map(t => t.player9);
+			player10list = player10list.map(t => t.player10);
 
 			console.log(rowlist);
 			tagString = rowlist.join(', ') || 'Nothing logged...';
@@ -36,7 +50,7 @@ module.exports = {
 		// equivalent to: SELECT * FROM tags WHERE name = 'tagID' LIMIT 1;
 		const tag = await Tags.findOne({ where: { rowid: tagID } });
 		if (tag) {
-			return message.reply( "\n" +
+			return message.reply("\n" +
 				"ID: " + tag.get('rowid') + "\n" +
 				"Game: " + tag.get('gameName') + "\n" +
 				"Date: " + tag.get('date') + "\n" +
